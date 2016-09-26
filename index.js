10cdbd0db9661b3bc9dfa4d2b636bdca984fbba4
Ray Villalobos <ray@lynda.com>
2016-09-25 22:11:40 -0400

    01_04e Managing Window Events

--- index.js
+++ index.js
@@ -5,15 +5,26 @@ var app = electron.app;
 
 app.on('ready', function() {
   var appWindow, infoWindow;
-  appWindow = new BrowserWindow();
+  appWindow = new BrowserWindow({
+    show: false
+  });
   appWindow.loadURL('http://raybo.org');
 
   infoWindow = new BrowserWindow({
     width: 400,
     height: 300,
     transparent: true,
+    show: false,
     frame: false
   });
 
   infoWindow.loadURL('file://' + __dirname + '/info.html');
+
+  appWindow.once('ready-to-show', function() {
+    appWindow.show();
+    setTimeout(function() {
+      infoWindow.show();
+      setTimeout(function() { infoWindow.hide();}, 3000);
+    }, 1000);
+  });
 });
