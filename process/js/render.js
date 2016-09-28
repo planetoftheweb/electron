cbd54681c374361ba26fabd3f7a1d460f398ccb9
Ray Villalobos <ray@lynda.com>
2016-09-28 18:13:49 -0400

    02_05e External Component

--- process/js/render.js
+++ process/js/render.js
@@ -1,10 +1,12 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-
 var $ = jQuery = require('jquery');
 var bootstrap = require('bootstrap');
 var fs = eRequire('fs');
 var loadApts = JSON.parse(fs.readFileSync(dataLocation));
+
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var AptList = require('./AptList');
 
 var MainInterface = React.createClass({
   getInitialState: function() {
@@ -14,33 +16,25 @@ var MainInterface = React.createClass({
   }, //getInitialState
   render: function() {
     var myAppointments = this.state.myAppointments;
-    return(
 
+    myAppointments=myAppointments.map(function(item, index) {
+      return(
+        <AptList key = {index}
+          singleItem = {item}
+        />
+      ) // return
+    }.bind(this)); //Appointments.map
+    return(
       <div className="application">
         <div className="container">
          <div className="row">
            <div className="appointments col-sm-12">
              <h2 className="appointments-headline">Current Appointments</h2>
-             <ul className="item-list media-list">
-
-               <li className="pet-item media">
-                 <div className="pet-info media-body">
-                   <div className="pet-head">
-                     <span className="pet-name">{myAppointments[0].petName}</span>
-                     <span className="apt-date pull-right">{myAppointments[0].aptDate}</span>
-                   </div>
-                   <div className="owner-name"><span className="label-item">Owner:</span>
-                   {myAppointments[0].ownerName}</div>
-                   <div className="apt-notes">{myAppointments[0].aptNotes}</div>
-                 </div>
-               </li>
-
-             </ul>
+             <ul className="item-list media-list">{myAppointments}</ul>
            </div>{/* col-sm-12 */}
          </div>{/* row */}
         </div>{/* container */}
       </div>
-
     );
   } //render
 });//MainInterface
