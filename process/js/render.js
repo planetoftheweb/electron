c5f810c298db22edadfbfba3b5333d7250c4f4ee
Ray Villalobos <ray@lynda.com>
2016-09-27 22:41:38 -0400

    02_04e Importing files with NodeJS

--- process/js/render.js
+++ process/js/render.js
@@ -3,11 +3,44 @@ var ReactDOM = require('react-dom');
 
 var $ = jQuery = require('jquery');
 var bootstrap = require('bootstrap');
+var fs = eRequire('fs');
+var loadApts = JSON.parse(fs.readFileSync(dataLocation));
 
 var MainInterface = React.createClass({
+  getInitialState: function() {
+    return {
+      myAppointments: loadApts
+    }//return
+  }, //getInitialState
   render: function() {
+    var myAppointments = this.state.myAppointments;
     return(
-      <h1>Wisdom Pet Application</h1>
+
+      <div className="application">
+        <div className="container">
+         <div className="row">
+           <div className="appointments col-sm-12">
+             <h2 className="appointments-headline">Current Appointments</h2>
+             <ul className="item-list media-list">
+
+               <li className="pet-item media">
+                 <div className="pet-info media-body">
+                   <div className="pet-head">
+                     <span className="pet-name">{myAppointments[0].petName}</span>
+                     <span className="apt-date pull-right">{myAppointments[0].aptDate}</span>
+                   </div>
+                   <div className="owner-name"><span className="label-item">Owner:</span>
+                   {myAppointments[0].ownerName}</div>
+                   <div className="apt-notes">{myAppointments[0].aptNotes}</div>
+                 </div>
+               </li>
+
+             </ul>
+           </div>{/* col-sm-12 */}
+         </div>{/* row */}
+        </div>{/* container */}
+      </div>
+
     );
   } //render
 });//MainInterface
