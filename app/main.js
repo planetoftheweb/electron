059820b78ae8d7d361b02fb4ac5dee209df64b0f
Ray Villalobos <ray@lynda.com>
2016-10-02 00:28:35 -0400

    04_01e Custom Menus

--- app/main.js
+++ app/main.js
@@ -1,7 +1,9 @@
 var electron = require('electron');
 var BrowserWindow = electron.BrowserWindow;
+var Menu = electron.Menu;
 var app = electron.app;
 var ipc = electron.ipcMain;
+var myAppMenu, menuTemplate;
 
 app.on('ready', function() {
   var appWindow, infoWindow;
@@ -34,5 +36,33 @@ app.on('ready', function() {
     event.returnValue='';
     infoWindow.hide();
   }); //closeInfoWindow
+
+  menuTemplate = [
+    {
+      label: 'Wisdom Pet',
+      submenu: [
+        {
+          role: 'help',
+          label: 'Our Website',
+          click() { electron.shell.openExternal('http://raybo.org')}
+        },
+        {role: 'close'},
+        {role: 'quit'}
+      ]
+    },{
+      label: 'Edit',
+      submenu: [
+        {role: 'undo'},
+        {role: 'redo'},
+        {role: 'cut'},
+        {role: 'copy'},
+        {role: 'paste'},
+        {role: 'selectall'}
+      ]
+    }
+  ];
+
+  myAppMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(myAppMenu);
 
 }); //app is ready
